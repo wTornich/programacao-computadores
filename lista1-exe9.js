7bfc46acae670d913524305dab0ee2eb5a67b7bc
tornich <tornico.kw@ourlook.com>
2020-08-26 21:56:40 -0300

    exercicio 9 atualizado

--- lista1-exe9.js
+++ lista1-exe9.js
@@ -1,2 +1,21 @@
 
-console.log("Hellow")
+function exe9() {
+
+    var n = Number(prompt(`Digite um número inteiro positivo`))
+    while (n <= 0) {
+        alert(`Número negativo ou 0!`)
+        n = Number(prompt(`Digite um número inteiro positivo`))
+    }
+    exe9_1(n)
+}
+
+function exe9_1(n) {
+
+    for (var cont=n;cont>=1;cont--) {
+
+        if (n % n == 0) {
+            soma += n + cont
+        }
+
+    }
+}
