73329c1dd0c4c577b3dad39508c036b8bcdd8a9d
tornich <tornico.kw@ourlook.com>
2020-09-15 20:48:38 -0300

    exer matriz

--- exer_matriz.js
+++ exer_matriz.js
@@ -3,40 +3,69 @@
 function chefe() {
 
     let matriz = []
+    let vetor = []
 
-    matriz[0] = []
-    matriz[1] = []
-    matriz[2] = []
-    cadastra(matriz)
-    consultaMediaAlunos(matriz)
-
+    cadastra(matriz, vetor)
+    consultaMediaAlunos(matriz, vetor)
+    calculaMediaProvas(matriz)
+    calculaMaiorNotaProva(matriz, vetor)
 }
 
-function cadastra(mat) {
+function calculaMaiorNotaProva(mat, vet) {
+
+    for (let j=0;j<4;J++) {
+        let maiorNota = mat[0][j]
+        let nomaMaiorNota = vet[i].nome
+        for (let i=0;i<3;i++) {
+            if (mat[i][j] > maiorNota){
+                maiorNota = mat[i][j]
+                nomeMaiorNota = vet[i].nome
+            }
+        }
+        console.log(`A prova ${j+1} tem a maior nota ${maiorNota} do aluno ${nomeMaiorNota}`)
+
+    }
+}
+
+
+function cadastra(mat, vet) {
     for (let i=0;i<3;i++) {
-        alert(`Informe 4 notas do aluno ${i+1}`)
+        let objeto = new Object()
+        objeto.nome = prompt(`Informe nome`)
+        objeto.rg = prompt(`Informe RG`)
+        objeto.media = 0
+
+        vet.push(objeto)
+        mat[i] = []
+        alert(`Informe 4 notas do aluno ${objeto.nome}`)
         for (let j=0;j<4;j++) {
             mat[i][j] = Number(prompt(`Informe a nota ${j+1}`))
         }
     }
 }
 
-function consultaMediaAlunos(mat) {
+function consultaMediaAlunos(mat, vet) {
 
-    let vetor =[]
+    
 
     for (let i=0;i<3;i++) {
-        let soma=0
         for (let j=0;j<4;j++) {
-            soma = soma + mat[i][j]
+            vet[i].media = vet[i].media + mat[i][j]
         }
-        vetor.push(soma/4)
+        vet[i].media = vet[i].media/4
     }
 
-    let medias=``
-    for (let i=0;i<vetor.length;i++) {
-        medias += `Média ${i+1} = ${vetor[i]}\n`
+    console.log(vet)
+}
+
+function calculaMediaProvas(mat) {
+
+    for (let j=0;j<4;j++) {
+        let media=0
+        for (let i=0;i<3;i++) {
+            media = media + mat[i][j]
+        }
+        console.log(`A media da prova ${j+1} é ${media/3}`)
     }
 
-    alert(medias)
 }
